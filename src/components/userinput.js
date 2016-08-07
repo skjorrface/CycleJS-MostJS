65e8ad74729406ac99684f646a4710dabdb9abd9
skjorrface <Alberto.scorrano@gmail.com>
2016-08-07 17:20:03 +0200

    Update userinput.js

--- src/components/userinput.js
+++ src/components/userinput.js
@@ -1,22 +1,21 @@
-import most from 'most';
-import { div, label, input, h4 } from '@cycle/dom';
-import isolate from '@cycle/isolate';
+import {div, label, input} from '@cycle/dom'
+import isolate from '@cycle/isolate'
 
-
-const intent = domSource => domSource.DOM.select('.name').events('input');
-const model = intent => intent.map(userInput => userInput.target.value).startWith('');
-const view = model => model.map(
+const intent = domSource => domSource.DOM.select(`.name`).events(`input`)
+const model = intent$ => intent$.map(
+  userInput => userInput.target.value).startWith(``)
+const view = model$ => model$.map(
   userData =>
-    div('.name', [
+    div(`.name`, [
       label(`${userData}`),
-      input()
+      input(),
     ])
-);
+)
 function UserInput(sources) {
   const sinks = {
-    DOM: view(model(intent(sources)))
+    DOM: view(model(intent(sources))),
   }
-  return sinks;
+  return sinks
 }
 
-export default sources => isolate(UserInput)(sources);
+export default sources => isolate(UserInput)(sources)
